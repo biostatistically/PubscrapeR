acd1b2dc9c08a0e9f5cd70122dbc53209df4423f
Ivy Chen <62801526+biostatistically@users.noreply.github.com>
2020-11-05 00:15:18 -0500

    package development files added

---
+++ R/pubscraper--package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+#> [1] "_PACKAGE"
+
+#' @import XML
+#' @import businessPubMed
+#' @import easyPubMed
+#' @import dplyr
+#' @importFrom tidyr drop_na
+#' @importFrom here here
+
